0782825aaf24c37bcbcdc9c16d8ca0a6f1f1963e
Brennan LeClair <94870273+Blec333@users.noreply.github.com>
2022-05-26 22:25:56 -0700

    add web socket framework

--- server/server.js
+++ server/server.js
@@ -45,3 +45,23 @@ db.once('open', () => {
     console.log(`API server for ${activity} running on port ${PORT}!`);
   });
 });
+
+
+
+// HANDLE WEB SOCKETS BELOW ----------------------------------------------------------------------------------
+const http = require('http').createServer();
+
+const io = require('socket.io')(http, {
+    cors: { origin: "*" }
+});
+
+io.on('connection', (socket) => {
+    console.log('a user connected');
+
+    socket.on('message', (message) =>     {
+        console.log(message);
+        io.emit('message', `${socket.id.substr(0,2)} said ${message}` );   
+    });
+});
+
+http.listen(8080, () => console.log('listening on http://localhost:8080') );
