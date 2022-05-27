da64d223ef6ed7dac695ba69e6613fc319054db1
Brennan LeClair <94870273+Blec333@users.noreply.github.com>
2022-05-27 09:12:14 -0700

    socket update

--- server/server.js
+++ server/server.js
@@ -1,19 +1,18 @@
 const path = require("path");
 const express = require('express');
 const db = require('./config/connection');
-const routes = require('./routes');
 const session = require("express-session");
-const exphbs = require("express-handlebars");
-const routes = require("./controllers");
+// const exphbs = require("express-handlebars");
+// const routes = require("./controllers");
 
-const MongodbStore = require("connect-mongodb-session")(session.Store);
+// const MongodbStore = require("connect-mongodb-session")(session.Store);
 
 
 const cwd = process.cwd();
 
 const PORT = process.env.PORT || 3001;
 const app = express();
-const hbs = exphbs.create();
+// const hbs = exphbs.create();
 
 // Note: not necessary for the Express server to function. This just helps indicate what activity's server is running in the terminal.
 const activity = cwd.includes('01-Activities')
@@ -21,24 +20,24 @@ const activity = cwd.includes('01-Activities')
   : cwd;
 
 
-  const sess = {
-    secret: "SuperDuper Secreter Secret Secret",
-    cookie: { maxAge: 1000000 },
-    resave: false,
-    saveUninitialized: true,
-    store: new SequelizeStore({
-      db: sequelize,
-    }),
-  };
+  // const sess = {
+  //   secret: "SuperDuper Secreter Secret Secret",
+  //   cookie: { maxAge: 1000000 },
+  //   resave: false,
+  //   saveUninitialized: true,
+  //   store: new SequelizeStore({
+  //     db: sequelize,
+  //   }),
+  // };
 
 
-  app.engine("handlebars", hbs.engine);
-  app.set("view engine", "handlebars");
+  // app.engine("handlebars", hbs.engine);
+  // app.set("view engine", "handlebars");
 
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use(routes);
+// app.use(routes);
 
 db.once('open', () => {
   app.listen(PORT, () => {
