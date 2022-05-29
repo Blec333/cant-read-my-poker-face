30099aa2520426281c4d8b1eaa28b8b74e8fb906
callanhunter <callan57hunter@gmail.com>
2022-05-29 10:03:23 -0700

    tailwind installation

--- client/src/index.js
+++ client/src/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
-import "bootstrap/dist/css/bootstrap.min.css";
+import "./src/**/*.{js,jsx,ts,tsx}";
 
 ReactDOM.render(<App />, document.getElementById("root"));
