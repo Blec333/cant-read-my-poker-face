12d985231092692e7f3e93de4d7f6e2359b037e3
Brennan LeClair <94870273+Blec333@users.noreply.github.com>
2022-06-05 13:09:36 -0700

    add typedefs and resolvers to server side

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,5 +1,5 @@
 const { AuthenticationError } = require('apollo-server-express');
-const { Player } = require('../models');
+const { Player, Game, Location } = require('../models');
 const { signToken } = require('../utils/auth');
 
 const resolvers = {
@@ -7,42 +7,69 @@ const resolvers = {
     players: async () => {
       return Player.find();
     },
-
     player: async (parent, { playerId }) => {
       return Player.findOne({ _id: playerId });
+    },
+
+    games: async () => {
+      return Game.find();
+    },
+    game: async (parent, { gameId }) => {
+      return Game.findOne({ _id: gameId });
+    },
+
+    locations: async () => {
+      return Location.find();
+    },
+    location: async (parent, { locationId }) => {
+      return Location.findOne({ _id: locationId });
     },
   },
 
   // 🔑 We call the signToken() function in the resolvers where we want to transmit data securely to generate a signed token:
   Mutation: {
-    addPlayer: async (parent, { playerName, password }) => {
-      const player = await Player.create({ playerName, password });
+    login: async (parent, { playerId, password }) => {
+      const player = await Player.findOne({ _id: playerId });
+      if (!player) {
+        throw new AuthenticationError('No player with this name found!');
+      }
+      const correctPw = await player.isCorrectPassword(password);
+      if (!correctPw) {
+        throw new AuthenticationError('Incorrect password!');
+      }
+      const token = signToken(player);
+      return { token, player };
+    },
+
+
+    addPlayer: async (parent, { playerName, password, account }) => {
+      const player = await Player.create({ playerName, password, account });
       const token = signToken(player);
       console.log({ token, player });
       return { token, player };
     },
-    login: async (parent, { playerId, password }) => {
-      const player = await Player.findOne({ _id: playerId });
-
-      if (!player) {
-        throw new AuthenticationError('No player with this name found!');
-      }
-
-      const correctPw = await player.isCorrectPassword(password);
-
-      if (!correctPw) {
-        throw new AuthenticationError('Incorrect password!');
-      }
-
-      const token = signToken(player);
-      return { token, player };
+    removePlayer: async (parent, { playerId }) => {
+      return Player.findOneAndDelete({ _id: playerId });
     },
 
-    addGame: async (parent, { playerId, game }) => {
-      return Player.findOneAndUpdate(
-        { _id: playerId },
+
+    addGame: async (parent, { playerId, winner, playerLimit, type }) => {
+      return game = await Game.create({ winner, playerLimit, type },
         {
-          $addToSet: { games: game },
+          $addToSet: { players: playerId },
+        },
+        )
+    },
+    removeGame: async (parent, { gameId }) => {
+      return Game.findOneAndDelete({ _id: gameId });
+    },
+
+
+    addPlayerToGame: async (parent, { gameId, playerId }) => {
+      return await Game.findOneAndUpdate(
+        { _id: gameId },
+        {
+          $addToSet: { players: playerId },
         },
         {
           new: true,
@@ -50,16 +77,36 @@ const resolvers = {
         }
       );
     },
-    removePlayer: async (parent, { playerId }) => {
-      return Player.findOneAndDelete({ _id: playerId });
-    },
-    removeGame: async (parent, { playerId, game }) => {
-      return Player.findOneAndUpdate(
-        { _id: playerId },
-        { $pull: { games: game } },
+    removePlayerFromGame: async (parent, { gameId, playerId }) => {
+      return Game.findOneAndUpdate(
+        { _id: gameId },
+        { $pull: { players: playerId } },
         { new: true }
       );
     },
+
+
+    addGameToPlayer: async (parent, { playerId, gameId }) => {
+      return await Player.findOneAndUpdate(
+        { _id: playerId },
+        {
+          $addToSet: { games: gameId },
+        },
+        {
+          new: true,
+          runValidators: true,
+        }
+      );
+    },
+    removeGameFromPlayer: async (parent, { playerId, gameId }) => {
+      return Player.findOneAndUpdate(
+        { _id: playerId },
+        { $pull: { games: gameId } },
+        { new: true }
+      );
+    },
+
+    
   },
 };
 
