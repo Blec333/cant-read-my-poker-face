23fefa31c680ba9c64c2a3f7eaa9ae93b8b03c2b
callanhunter <callan57hunter@gmail.com>
2022-06-07 19:29:56 -0700

    working on login functionality

--- client/src/components/pages/Login.jsx
+++ client/src/components/pages/Login.jsx
@@ -7,16 +7,16 @@ import SignUp from "./SignUp";
 import Auth from "../../utils/auth";
 
 const Login = (props) => {
-  const [formState, setFormState] = useState({ playerName: "", password: "" });
+  const [formState, setFormState] = useState({ username: "", password: "" });
   const [login, { error, data }] = useMutation(LOGIN_USER);
 
   // update state based on form input changes
   const handleChange = (event) => {
-    const { playerName, value } = event.target;
+    const { username, value } = event.target;
 
     setFormState({
       ...formState,
-      [playerName]: value,
+      [username]: value,
     });
   };
 
@@ -36,7 +36,7 @@ const Login = (props) => {
 
     // clear form values
     setFormState({
-      playerName: "",
+      username: "",
       password: "",
     });
   };
@@ -45,120 +45,96 @@ const Login = (props) => {
     <div class="bg-white h-screen w-screen flex justify-center items-center">
       <div class="px-6 py-3 rounded border w-64">
         <div class="flex flex-col items-center justify-center mb-4">
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            class="w-12 h-12"
-            fill="none"
-            viewBox="0 0 24 24"
-            stroke="currentColor"
-            stroke-width="2"
-          >
-            <path
-              stroke-linecap="round"
-              stroke-linejoin="round"
-              d="M12 15v2m-6 4h12a2 2 0 002-2v-6a2 2 0 00-2-2H6a2 2 0 00-2 2v6a2 2 0 002 2zm10-10V7a4 4 0 00-8 0v4h8z"
-            />
-          </svg>
-          <h2 class="text-2xl font-bold">Login</h2>
-        </div>
-        {data ? (
-          <p>
-            Success! You may now head <Link to="/">back to the homepage.</Link>
-          </p>
-        ) : (
-          <form onSubmit={handleFormSubmit}>
-            {/* playerName */}
-            <div class="flex flex-col my-2">
-              <label class="text-xs text-gray-400">Username</label>
-              <div class="text-xs text-red-400 flex justify-between items-center">
-                {/* <span>
-                    <b>Error: </b>
-                    wrong playerName
-                    </span> */}
+          <div className="card-body">
+            {" "}
+            {data ? (
+              <p>
+                Success! You may now head{" "}
+                <Link to="/">back to the homepage.</Link>
+              </p>
+            ) : (
+              <form onSubmit={handleFormSubmit}>
+                <input
+                  className="form-input"
+                  placeholder="Your player name"
+                  name="playerName"
+                  type="playerName"
+                  value={formState.username}
+                  onChange={handleChange}
+                />
+                <input
+                  className="form-input"
+                  placeholder="******"
+                  name="password"
+                  type="password"
+                  value={formState.password}
+                  onChange={handleChange}
+                />
+                <button
+                  className="btn btn-block btn-primary"
+                  style={{ cursor: "pointer" }}
+                  type="submit"
+                >
+                  Submit
+                </button>
+              </form>
+            )}
+            {error && (
+              <div className="my-3 p-3 bg-danger text-white">
+                {error.message}
               </div>
-              <input
-                class="border rounded px-3 py-1 mt-2"
-                className="form-input"
-                name=""
-                type="text"
-              />
-            </div>
-            <div class="flex flex-col my-2">
-              <label class="text-xs text-gray-400">Password</label>
-              <input
-                class="border rounded px-3 py-1 mt-2"
-                className="form-input"
-                placeholder="******"
-                name="password"
-                type="password"
-              />
-            </div>
-            <div class="flex flex-col items-center justify-center my-3">
-              <button class="my-3 py-1 w-full rounded bg-blue-600 text-blue-200">
-                Submit
-              </button>
-              {/* <p class="text-xs text-gray-500">
-                    Forgot password ? 
-                        <a href="#" class="font-bold text-gray-700">Click here</a> 
-                        to reset your password.
-                </p> */}
-            </div>
-            <div>Don't have an account?</div>
-            <Link className="text-blue-400" to="./SignUp">
-              Sign Up
-            </Link>
-          </form>
-        )}
+            )}
+          </div>
+        </div>
       </div>
-    </div>
-  );
-  // <main className="flex-row justify-center mb-4">
-  //   <div className="col-12 col-lg-10">
-  //     <div className="card">
-  //       <h4 className="card-header bg-dark text-light p-2">Login</h4>
-  //       <div className="card-body">
-  //         {data ? (
-  //           <p>
-  //             Success! You may now head{' '}
-  //             <Link to="/">back to the homepage.</Link>
-  //           </p>
-  //         ) : (
-  //           <form onSubmit={handleFormSubmit}>
-  //             <input
-  //               className="form-input"
-  //               placeholder="Your player name"
-  //               name="playerName"
-  //               type="playerName"
-  //               value={formState.playerName}
-  //               onChange={handleChange}
-  //             />
-  //             <input
-  //               className="form-input"
-  //               placeholder="******"
-  //               name="password"
-  //               type="password"
-  //               value={formState.password}
-  //               onChange={handleChange}
-  //             />
-  //             <button
-  //               className="btn btn-block btn-primary"
-  //               style={{ cursor: 'pointer' }}
-  //               type="submit"
-  //             >
-  //               Submit
-  //             </button>
-  //           </form>
-  //         )}
+    </div> // <main className="flex-row justify-center mb-4">
+    //   <div className="col-12 col-lg-10">
+    //     <div className="card">
+    //       <h4 className="card-header bg-dark text-light p-2">Login</h4>
+    //       <div className="card-body">
+    //         {data ? (
+    //           <p>
+    //             Success! You may now head{' '}
+    //             <Link to="/">back to the homepage.</Link>
+    //           </p>
+    //         ) : (
+    //           <form onSubmit={handleFormSubmit}>
+    //             <input
+    //               className="form-input"
+    //               placeholder="Your player name"
+    //               name="playerName"
+    //               type="playerName"
+    //               value={formState.playerName}
+    //               onChange={handleChange}
+    //             />
+    //             <input
+    //               className="form-input"
+    //               placeholder="******"
+    //               name="password"
+    //               type="password"
+    //               value={formState.password}
+    //               onChange={handleChange}
+    //             />
+    //             <button
+    //               className="btn btn-block btn-primary"
+    //               style={{ cursor: 'pointer' }}
+    //               type="submit"
+    //             >
+    //               Submit
+    //             </button>
+    //           </form>
+    //         )}
 
-  //         {error && (
-  //           <div className="my-3 p-3 bg-danger text-white">
-  //             {error.message}
-  //           </div>
-  //         )}
-  //       </div>
-  //     </div>
-  //   </div>
-  // </main>
+    //         {error && (
+    //           <div className="my-3 p-3 bg-danger text-white">
+    //             {error.message}
+    //           </div>
+    //         )}
+    //       </div>
+    //     </div>
+    //   </div>
+    // </main>
+  );
 };
 
 export default Login;
